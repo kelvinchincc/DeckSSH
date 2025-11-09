c9a14346b2717ef47f4dcfc3643effd37d4c730a
Kelvin Chin <chinkaiwen_35@outlook.com>
2025-11-09 17:38:44 +0800

    Initialized repo

---
+++ mainwindow.cpp
@@ -0,0 +1,8 @@
+#include "mainwindow.hpp"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui{std::make_unique<Ui::MainWindow>()}
+{
+    ui->setupUi(this);
+}
---
+++ mainwindow.hpp
@@ -0,0 +1,25 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <QMainWindow>
+#include <memory>
+
+#include "./ui_mainwindow.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+
+private:
+    std::unique_ptr<Ui::MainWindow> ui{nullptr};
+};
+#endif // MAINWINDOW_HPP
